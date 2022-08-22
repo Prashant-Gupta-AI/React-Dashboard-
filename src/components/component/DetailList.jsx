700f518736982a41bb947cae80b60f7e51cf5dcb
Prashant <prashantgupta852@gmail.com>
2022-08-22 21:02:29 +0530

    Added Responsive layout

--- src/components/component/DetailList.jsx
+++ src/components/component/DetailList.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../styles/detaillist.css'
+import image from '../images/user.webp'
 
 function DetailList() {
     return (
         <>
             <div className="details">
+                {/* Order Details list */}
                 <div className="recentOrders">
                     <div className="cardHeader">
                         <h2>Recent Orders</h2>
@@ -26,6 +28,155 @@ function DetailList() {
                                 <td>Paid</td>
                                 <td><span className='status delivered'>Delivered</span></td>
                             </tr>
+                            <tr>
+                                <td>Car</td>
+                                <td>$12000</td>
+                                <td>Paid</td>
+                                <td><span className='status pending'>pending</span></td>
+                            </tr>
+                            <tr>
+                                <td>Car</td>
+                                <td>$12000</td>
+                                <td>Paid</td>
+                                <td><span className='status return'>return</span></td>
+                            </tr>
+                            <tr>
+                                <td>Car</td>
+                                <td>$12000</td>
+                                <td>Paid</td>
+                                <td><span className='status inprogress'>inprogress</span></td>
+                            </tr>
+                            <tr>
+                                <td>Car</td>
+                                <td>$12000</td>
+                                <td>Paid</td>
+                                <td><span className='status delivered'>Delivered</span></td>
+                            </tr>
+                            <tr>
+                                <td>Car</td>
+                                <td>$12000</td>
+                                <td>Paid</td>
+                                <td><span className='status delivered'>Delivered</span></td>
+                            </tr>
+                            <tr>
+                                <td>Car</td>
+                                <td>$12000</td>
+                                <td>Paid</td>
+                                <td><span className='status delivered'>Delivered</span></td>
+                            </tr>
+                            <tr>
+                                <td>Car</td>
+                                <td>$12000</td>
+                                <td>Paid</td>
+                                <td><span className='status delivered'>Delivered</span></td>
+                            </tr>
+                            <tr>
+                                <td>Car</td>
+                                <td>$12000</td>
+                                <td>Paid</td>
+                                <td><span className='status delivered'>Delivered</span></td>
+                            </tr>
+                            <tr>
+                                <td>Car</td>
+                                <td>$12000</td>
+                                <td>Paid</td>
+                                <td><span className='status delivered'>Delivered</span></td>
+                            </tr>
+
+                            <tr>
+                                <td>Car</td>
+                                <td>$12000</td>
+                                <td>Paid</td>
+                                <td><span className='status delivered'>Delivered</span></td>
+                            </tr>
+                            <tr>
+                                <td>Car</td>
+                                <td>$12000</td>
+                                <td>Paid</td>
+                                <td><span className='status delivered'>Delivered</span></td>
+                            </tr>
+                            <tr>
+                                <td>Car</td>
+                                <td>$12000</td>
+                                <td>Paid</td>
+                                <td><span className='status delivered'>Delivered</span></td>
+                            </tr>
+
+                            <tr>
+                                <td>Car</td>
+                                <td>$12000</td>
+                                <td>Paid</td>
+                                <td><span className='status delivered'>Delivered</span></td>
+                            </tr>
+                            <tr>
+                                <td>Car</td>
+                                <td>$12000</td>
+                                <td>Paid</td>
+                                <td><span className='status delivered'>Delivered</span></td>
+                            </tr>
+                            <tr>
+                                <td>Car</td>
+                                <td>$12000</td>
+                                <td>Paid</td>
+                                <td><span className='status delivered'>Delivered</span></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+                {/*  New Custumers */}
+                <div className="recentCustumers">
+                    <div className="cardHeader">
+                        <h2>Recent Customers</h2>
+                    </div>
+                    <table>
+                        <tbody>
+                        <tr>
+                            <td width="60px"><div className="imgBx"><img src={image} alt="user" /></div></td>
+                            <td><h4>User_Name<br/><span>location</span></h4></td>
+                        </tr>
+                        <tr>
+                            <td width="60px"><div className="imgBx"><img src={image} alt="user" /></div></td>
+                            <td><h4>User_Name<br/><span>location</span></h4></td>
+                        </tr>
+                        <tr>
+                            <td width="60px"><div className="imgBx"><img src={image} alt="user" /></div></td>
+                            <td><h4>User_Name<br/><span>location</span></h4></td>
+                        </tr>
+                        <tr>
+                            <td width="60px"><div className="imgBx"><img src={image} alt="user" /></div></td>
+                            <td><h4>User_Name<br/><span>location</span></h4></td>
+                        </tr>
+                        <tr>
+                            <td width="60px"><div className="imgBx"><img src={image} alt="user" /></div></td>
+                            <td><h4>User_Name<br/><span>location</span></h4></td>
+                        </tr>
+                        <tr>
+                            <td width="60px"><div className="imgBx"><img src={image} alt="user" /></div></td>
+                            <td><h4>User_Name<br/><span>location</span></h4></td>
+                        </tr>
+                        <tr>
+                            <td width="60px"><div className="imgBx"><img src={image} alt="user" /></div></td>
+                            <td><h4>User_Name<br/><span>location</span></h4></td>
+                        </tr>
+                        <tr>
+                            <td width="60px"><div className="imgBx"><img src={image} alt="user" /></div></td>
+                            <td><h4>User_Name<br/><span>location</span></h4></td>
+                        </tr>
+                        <tr>
+                            <td width="60px"><div className="imgBx"><img src={image} alt="user" /></div></td>
+                            <td><h4>User_Name<br/><span>location</span></h4></td>
+                        </tr>
+                        <tr>
+                            <td width="60px"><div className="imgBx"><img src={image} alt="user" /></div></td>
+                            <td><h4>User_Name<br/><span>location</span></h4></td>
+                        </tr>
+                        
+                      
+                     
+        
+                        
+        
                         </tbody>
                     </table>
                 </div>
@@ -35,4 +186,4 @@ function DetailList() {
     )
 }
 
-export default DetailList
+export default DetailList;
